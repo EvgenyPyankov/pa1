b389050936a471190766b0207f214962d5cd3bf4
Georgii Sirotkin <georgiysirotkin@gmail.com>
2016-05-07 14:16:00 +0300

    Create n + 1 pipes, create n children

--- main.c
+++ main.c
@@ -14,7 +14,11 @@
 enum { NUM_CHILDREN = 10 }; ///
 enum { NUM_MESSAGES = 10 }; ///
 
-static int write_pipes[NUM_CHILDREN]; ///
+enum { MAX_NUMBER_OF_CHILDREN = 10 };
+
+static int write_pipes[MAX_NUMBER_OF_CHILDREN + 1];
+static int read_pipes[MAX_NUMBER_OF_CHILDREN + 1];
+
 static int n_pipes; ///
 
 
@@ -23,22 +27,22 @@ int childrenNumber;
 FILE* eventsLog;
 FILE* pipesLog;
 
-static void be_childish(int *pipe) ////
+static void be_childish(int id) ////
 {
-    int i;
-    char buffer[32];
-    int nbytes;
-    int pid = getpid();
-    close(pipe[1]);
-    for (i = 0; i < n_pipes; i++)
-        close(write_pipes[i]);
-    printf("Child %d\n", pid);
-    while ((nbytes = read(pipe[0], buffer, sizeof(buffer))) > 0)
-    {
-        printf("Child %d: %d %.*s\n", pid, nbytes, nbytes, buffer);
-        fflush(0);
-    }
-    printf("Child %d: finished\n", pid);
+    // int i;
+    // char buffer[32];
+    // int nbytes;
+    // int pid = getpid();
+    // close(pipe[1]);
+    // for (i = 0; i < n_pipes; i++)
+    //     close(write_pipes[i]);
+    // printf("Child %d\n", pid);
+    // while ((nbytes = read(pipe[0], buffer, sizeof(buffer))) > 0)
+    // {
+    //     printf("Child %d: %d %.*s\n", pid, nbytes, nbytes, buffer);
+    //     fflush(0);
+    // }
+    // printf("Child %d: finished\n", pid);
     exit(0);
 }
 
@@ -65,56 +69,96 @@ void eventsLogWrite(char* line)
 }
 
 int main(int argc, char **argv) {
-	pid_t pid;
-    int i, j;
+	childrenNumber = atoi(argv[2]);
 
-    /* Create the pipes and the children. */
-    for (i = 0; i < NUM_CHILDREN; i++)
+	pid_t pid;
+    int i;
+
+    /* Create pipes */
+    for (i = 0; i < childrenNumber + 1; i ++)
     {
-        int new_pipe[2];
-        if (pipe(new_pipe))
+    	int new_pipe[2];
+    	if (pipe(new_pipe))
         {
             int errnum = errno;
             fprintf(stderr, "Pipe failed (%d: %s)\n", errnum, strerror(errnum));
             return EXIT_FAILURE;
         }
-        if ((pid = fork()) < 0)
+        read_pipes[i] = new_pipe[0];
+        write_pipes[i] = new_pipe[1];
+    }
+
+    /* Create children. */
+    for (i = 1; i <= childrenNumber; i ++)
+    {
+    	pid = fork();
+
+    	if (pid < 0)
         {
             int errnum = errno;
             fprintf(stderr, "Fork failed (%d: %s)\n", errnum, strerror(errnum));
             return EXIT_FAILURE;
         }
-        else if (pid == 0)
+
+        if (pid == 0)
         {
-            be_childish(new_pipe);
+        	be_childish(i);
+        } else {
+        	close(read_pipes[i]);
+        	close(write_pipes[i]);
         }
-        else
-        {
-            close(new_pipe[0]);
-            write_pipes[n_pipes++] = new_pipe[1];
-        }
+        close(write_pipes[0]);
     }
 
-    for (i = 0; i < NUM_MESSAGES; i++)
-    {
-        char message[30];
-        int len;
-        snprintf(message, sizeof(message), "Message %d", i);
-        len = strlen(message);
-        for (j = 0; j < n_pipes; j++)
-        {
-            if (write(write_pipes[j], message, len) != len)
-            {
-                /* Inferior error handling; first failure causes termination */
-                fprintf(stderr, "Write failed (child %d)\n", j);
-                exit(1);
-            }
-        }
-        sleep(1);
-    }
+    // for (i = 0; i < NUM_CHILDREN; i++)
+    // {
+    //     int new_pipe[2];
+    //     if (pipe(new_pipe))
+    //     {
+    //         int errnum = errno;
+    //         fprintf(stderr, "Pipe failed (%d: %s)\n", errnum, strerror(errnum));
+    //         return EXIT_FAILURE;
+    //     }
+    //     if ((pid = fork()) < 0)
+    //     {
+    //         int errnum = errno;
+    //         fprintf(stderr, "Fork failed (%d: %s)\n", errnum, strerror(errnum));
+    //         return EXIT_FAILURE;
+    //     }
+    //     else if (pid == 0)
+    //     {
+    //         be_childish(new_pipe);
+    //     }
+    //     else
+    //     {
+    //         close(new_pipe[0]);
+    //         write_pipes[n_pipes++] = new_pipe[1];
+    //     }
+    // }
+
+    // for (i = 0; i < NUM_MESSAGES; i++)
+    // {
+    //     char message[30];
+    //     int len;
+    //     snprintf(message, sizeof(message), "Message %d", i);
+    //     len = strlen(message);
+    //     for (j = 0; j < n_pipes; j++)
+    //     {
+    //         if (write(write_pipes[j], message, len) != len)
+    //         {
+    //             /* Inferior error handling; first failure causes termination */
+    //             fprintf(stderr, "Write failed (child %d)\n", j);
+    //             exit(1);
+    //         }
+    //     }
+    //     sleep(1);
+    // }
     printf("Parent complete\n");
 
     return 0;
+    // 
+    // 
+    // 
 	// childrenNumber = atoi(argv[2]);
  //    printf("p = %d\n", childrenNumber);
 	// const char *buf = "123456789";
