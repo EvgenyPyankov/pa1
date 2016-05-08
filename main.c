42efdc1aaadb8af9d50b5a3cc657107e813858e8
Georgii Sirotkin <georgiysirotkin@gmail.com>
2016-05-08 09:42:03 +0300

    Read and write Message struct through pipes

--- main.c
+++ main.c
@@ -7,11 +7,14 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <time.h>
 
 
 #include "common.h"
 #include "ipc.h"
 #include "pa1.h"
+
+// http://cboard.cprogramming.com/c-programming/119168-sending-structs-pointers-through-pipes.html
 
 enum { NUM_CHILDREN = 10 }; ///
 enum { NUM_MESSAGES = 10 }; ///
@@ -33,7 +36,12 @@ void pipeWrite(int src, int dest, Message* msg){
 }
 
 int receive(void * self, local_id from, Message * msg){
+	// int nbytes = read(read_pipes[0][2], buffer, sizeof(buffer));
+	// if (nbytes != -1) {
+	// 	return 0;
+	// }
 
+	// return -1;
 }
 
 int receive_any(void * self, Message * msg){
@@ -41,11 +49,26 @@ int receive_any(void * self, Message * msg){
 }
 
 int send(void * self, local_id dst, const Message * msg){
-    
+	int src = (int)self;
+    int nbytes = write(write_pipes[src][dst], msg, sizeof(*msg));
+    printf("%d\n", sizeof(*msg));
+    if (nbytes != -1) {
+    	return 0;
+    }
+
+    return -1;
 }
 
 int send_multicast(void * self, const Message * msg){
+	int src = (int)self;
+	for (int i = 0; i <= childrenNumber; i ++) {
+		int result = send((void *)src, i, msg);
+		if (result != 0) {
+			return -1;
+		}
+	}
 
+	return 0;
 }
 
 void logWrite(int descriptor, char buf[])
@@ -59,10 +82,20 @@ void be_childish(int id) ////
 	close(read_pipes[id][0]);
 	int pid = getpid();
 	int parentProcessId = getppid();
-
+	
 	char buf[64];
 	sprintf(buf, log_started_fmt, id, pid, parentProcessId);
     logWrite(eventsLogDescriptor, buf);
+
+    MessageHeader header = {MESSAGE_MAGIC, strlen(buf), STARTED, (int)time(NULL)};
+    Message message;
+    message.s_header = header;
+    sprintf(message.s_payload, buf, strlen(buf));
+    if (send((void *)id, 0, &message) != 0) {
+    	// printf("Send() failed");
+    }
+    exit(0);
+}
 
 	// "Child %d started\n", id);
     // int i;
@@ -79,13 +112,6 @@ void be_childish(int id) ////
     //     fflush(0);
     // }
     // printf("Child %d: finished\n", pid);
-    exit(0);
-}
-
-// int send_multicast(void * self, const Message * msg)
-// {
-
-// }
 
 void openLogFiles(){
 	eventsLogDescriptor = open(events_log, O_WRONLY | O_APPEND | O_CREAT, 0666);
@@ -123,24 +149,24 @@ int main(int argc, char **argv) {
     /* Create pipes */
     for (i = 0; i < childrenNumber + 1; i ++)
     {
-    	for (j = 0; j < i; j ++)
+    	for (j = 0; j < childrenNumber + 1; j ++)
     	{
-    		int new_pipe[2];
-    		if (pipe(new_pipe))
-	        {
-	            int errnum = errno;
-	            fprintf(stderr, "Pipe failed (%d: %s)\n", errnum, strerror(errnum));
-	            return EXIT_FAILURE;
+    		if (i != j) {
+	    		int new_pipe[2];
+	    		if (pipe(new_pipe))
+		        {
+		            int errnum = errno;
+		            fprintf(stderr, "Pipe failed (%d: %s)\n", errnum, strerror(errnum));
+		            return EXIT_FAILURE;
+		        }
+	    		read_pipes[j][i] = new_pipe[0];
+	    		write_pipes[i][j] = new_pipe[1];
+	            // char buf[64];
+	            // sprintf(buf, new_pipe_fmt, new_pipe[0],new_pipe[1]);
+	            // logWrite(pipesLogDescriptor, buf);
+	            // sprintf(buf, new_pipe_fmt, new_pipe[1],new_pipe[0]);
+	            // logWrite(pipesLogDescriptor, buf);
 	        }
-    		read_pipes[i][j] = new_pipe[0];
-    		read_pipes[j][i] = new_pipe[0];
-    		write_pipes[i][j] = new_pipe[1];
-    		write_pipes[j][i] = new_pipe[1];
-            char buf[64];
-            sprintf(buf, new_pipe_fmt, new_pipe[0],new_pipe[1]);
-            logWrite(pipesLogDescriptor, buf);
-            sprintf(buf, new_pipe_fmt, new_pipe[1],new_pipe[0]);
-            logWrite(pipesLogDescriptor, buf);
     	}
     }
 
@@ -160,13 +186,22 @@ int main(int argc, char **argv) {
         {
         	be_childish(i);
         } else {
-        	for (j = 1; j < childrenNumber; j ++)
-        	{
-        		close(write_pipes[0][j]);
-        	}
+    		close(write_pipes[0][i]);
         }
     }
 
+	int nbytes;
+	Message* msg = malloc(sizeof(*msg));
+ //    // for (int i=1; i <= childrenNumber; i++){
+ //    // 	printf("%d\n", read_pipes[0][i]);
+ //    	char buffer[64];
+	// if (receive((void *)0, 3, &message) != 0 ) {
+	// 	printf("receive() failed");
+	// }
+	read(read_pipes[0][2], msg, sizeof(*msg));
+	printf("%s", msg[0].s_payload);
+ //        printf("%s\n", buffer);
+ //    // }
 
     while(wait(NULL)>0) {
     }
